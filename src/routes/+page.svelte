85362d5fc10c67bf4b2e5637de8e5fcef5e174df
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-09-22 15:04:09 -0300

    Updating routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+    <title>Alura Cook</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/receitas">Ir para receitas</a>
---
+++ src/routes/receitas/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Alura Cook | Receitas</title>
+</svelte:head>
+
+<p>Aqui vem as receitas</p>
+<a href="/">Voltar para página principal</a>
